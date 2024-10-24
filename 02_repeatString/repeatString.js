4be32785a70cccdade3a44580109de0f8d1b8f16
Miguel Cohen <miguelcohenmendinhos@gmail.com>
2024-10-24 22:04:16 +0100

    Finish exercises 1, 2 and 3.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    if(numberOfTimes > 0){
+        const stringArray = new Array(string)
+            for(i = 1; i < numberOfTimes; i++){
+            stringArray.push(string)
+            }
+        return stringArray.join("");
+    } else if (numberOfTimes === 0){
+        return "";
+    } else {
+        return "ERROR"
+    }
 };
 
+repeatString("hey", 3)
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+let reversedStringCharacters;
 
-};
+function reverseString(string) {
+    reversedStringCharacters = ""
+     let stringCharacters =  string.split("");
+     for(let i = 0; i < string.length; i++){
+        reversedStringCharacters += stringCharacters.pop();
+     }
+     console.log(reversedStringCharacters);
+     return reversedStringCharacters;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
