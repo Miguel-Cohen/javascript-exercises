a140e4e143e5d0771681c4c79a753f376721f07c
Miguel Cohen <miguelcohenmendinhos@gmail.com>
2024-12-24 09:09:34 +0000

    Finish exercises 06  and 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(initFahrenheit) {
+    return Math.round(10*(initFahrenheit-32)/1.8)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(initCelsius) {
+    return Math.round(10*(32 + 1.8*initCelsius))/10;
 };
+
+// console.log(convertToFahrenheit(73.2) +  ", " + convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
